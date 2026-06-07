f5f8d79ad578f0de61c4bfb5be912f182f26c2ee
Azzam Azmi <riskakreasikomputer@gmail.com>
2026-06-07 15:19:30 +0700

    chore: publish livewire config and increase temporary upload max to 50MB

---
+++ config/livewire.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+
+    'class_namespace' => 'App\\Livewire',
+
+    'view_path' => resource_path('views/livewire'),
+
+    'layout' => 'components.layouts.app',
+
+    'lazy_placeholder' => null,
+
+    'temporary_file_upload' => [
+        'disk' => null,
+        // Increase default max from 12288 KB (12MB) to 51200 KB (50MB)
+        'rules' => ['required', 'file', 'max:51200'],
+        'directory' => null,
+        'middleware' => null,
+        'preview_mimes' => [
+            'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
+            'mov', 'avi', 'wmv', 'mp3', 'm4a',
+            'jpg', 'jpeg', 'mpga', 'webp', 'wma',
+        ],
+        'max_upload_time' => 5,
+        'cleanup' => true,
+    ],
+
+    'render_on_redirect' => false,
+
+    'legacy_model_binding' => false,
+
+    'inject_assets' => true,
+
+    'navigate' => [
+        'show_progress_bar' => true,
+        'progress_bar_color' => '#2299dd',
+    ],
+
+    'inject_morph_markers' => true,
+
+    'smart_wire_keys' => false,
+
+    'pagination_theme' => 'tailwind',
+
+    'release_token' => 'a',
+];
